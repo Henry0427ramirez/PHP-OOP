929ff65a6e31686b97815c3f46fd7bac12c1256e
Henry Ramirez <h.ramirez0427@gmail.com>
2015-01-07 09:09:14 -0800

    commit 1
    this is my first example

---
+++ constructor.php
@@ -0,0 +1,17 @@
+<?php 
+  class Giraffe {
+  	public $age;
+  	public $size;
+  	public $name;
+
+  	       function__construct($age,$size,$name){
+  	       	$this->age = $age;
+  	       	$this->size = $size;
+  	       	$this->name = $name;
+  	       }
+
+  	       function myAge(){
+  	       	return"($this->age)" . "($this->)"
+  	       }
+  }
+?>
