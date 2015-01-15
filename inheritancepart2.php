f2116256876cdd4c50296f7a00f6314b2115d73f
Henry Ramirez <h.ramirez0427@gmail.com>
2015-01-14 22:10:21 -0800

    commit 7
    lesson 5

---
+++ inheritancepart2.php
@@ -0,0 +1,38 @@
+<?php
+
+class Child {
+	public $firstname;
+	public $lastname;
+	public $gender;
+	public $weight;
+	public $height;
+
+	function__construct($firstname,$lastname,$gender,$weight,$height) {
+		$this->firstname = $firstname;
+		$this->lastname = $lastname;
+		$this->gender = $gender;
+		$this->weight = $weight;
+		$this->height = $height;
+	}
+
+	function getName(){
+		return "This is " . $this->firstname . $this->lastname . " and its a " . $this->gender. ".";
+	}
+}
+class Boy extends Child {
+	function__construct($firstname,$lastname,$gender,$weight,$height)
+	parent::__construct($firstname,$lastname,$gender,$weight,$height);
+	$this->weight = $weight;
+}
+function greet(){
+	return $this->gender;
+}
+
+class Girl extends Child {
+	function__construct($firstname,$lastname,$gender,$weight,$height);
+	parent::__construct($firstname,$lastname,$gender,$weight,$height);
+	this->gender =$gender;
+}
+
+$Boy = new Boy("Boy","nice","brown","male", 15 true);
+print "Child 1 is a " . $Boy->getName();
